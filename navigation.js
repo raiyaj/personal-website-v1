32560b7b033befe979a1a734baf8b4a106ae7bca
raiyaj <raiyajessa@gmail.com>
2017-05-06 03:41:43 -0700

    create sliding (hamburger) menu for mobile

--- navigation.js
+++ navigation.js
@@ -3,7 +3,7 @@ $(document).ready(function (){
 
   // Declare size-related variables
   var scroll;  //pixels you've scrolled from top
-  var docwidth = $(document).width();
+  var docwidth;
   var homeHeight;  //height of div in pixels
   var aboutHeight;
   var navHeight = $('nav ul').height(); //always constant so calculate now
@@ -13,58 +13,62 @@ $(document).ready(function (){
   $(window).scroll(function() { // Execute each time you scroll
     scroll = $(window).scrollTop();
     homeHeight = $('#home').height();
-    if (scroll > 100) { // if you've scrolled further than the height of div
-      $('nav').css('background','rgba(242,242,242,0.9)');
-      $('nav ul li a').css({
-        'color': '#878787',
-        'padding': '15px 13px 7px 13px'
-      });
-    } else {
-      $('nav').css('background','none');
-      $('nav ul li a').css({
-        'color': 'white',
-        'padding': '35px 13px 7px 13px'
-      });
+    if ($(document).width() >= 800) {
+      if (scroll > 100) { // if you've scrolled further than the height of div
+        $('nav').css('background','rgba(242,242,242,0.9)');
+        $('nav ul li a').css({
+          'color': '#878787',
+          'padding': '15px 13px 7px 13px'
+        });
+      } else {
+        $('nav').css('background','none');
+        $('nav ul li a').css({
+          'color': 'white',
+          'padding': '35px 13px 7px 13px'
+        });
+      }
     }
   });
 
 
-  // Active Navigation
+  // Active navigation for desktop
   $(window).scroll(function() {
     scroll = $(window).scrollTop();
     homeHeight = $("#home").height();
     aboutHeight = $("#about").height();
     var distanceFromBottom = $(document).height() - scroll - $(window).height();
-    if (scroll < homeHeight-75) {
-      if (scroll <= 100) {
-        $('nav ul #home-nav').removeClass('active-grey');
-        $('nav ul #home-nav').addClass('active-white');
+    if ($(document).width() >= 800) {
+      if (scroll < homeHeight-75) {
+        if (scroll <= 100) {
+          $('nav ul #home-nav').removeClass('active-grey');
+          $('nav ul #home-nav').addClass('active-white');
+        } else {
+          $('nav ul #home-nav').addClass('active-white');
+          $('nav ul #home-nav').addClass('active-grey');
+        }
       } else {
-        $('nav ul #home-nav').addClass('active-white');
-        $('nav ul #home-nav').addClass('active-grey');
+        $('nav ul #home-nav').removeClass('active-white');
+        $('nav ul #home-nav').removeClass('active-grey');
       }
-    } else {
-      $('nav ul #home-nav').removeClass('active-white');
-      $('nav ul #home-nav').removeClass('active-grey');
-    }
-    if (scroll >= homeHeight-75 && scroll < homeHeight+aboutHeight+60) {
-      $('nav ul #about-nav').addClass('active-grey');
-    } else {
-      $('nav ul #about-nav').removeClass('active-grey');
-    }
-    if (scroll >= homeHeight+aboutHeight+60) {
-      $('nav ul #projects-nav').addClass('active-grey');
-    } else {
-      $('nav ul #projects-nav').removeClass('active-grey');
-    }
-    if (distanceFromBottom == 0) {
-      $('nav ul #about-nav').removeClass('active-grey');
-      $('nav ul #projects-nav').addClass('active-grey');
+      if (scroll >= homeHeight-75 && scroll < homeHeight+aboutHeight+60) {
+        $('nav ul #about-nav').addClass('active-grey');
+      } else {
+        $('nav ul #about-nav').removeClass('active-grey');
+      }
+      if (scroll >= homeHeight+aboutHeight+60) {
+        $('nav ul #projects-nav').addClass('active-grey');
+      } else {
+        $('nav ul #projects-nav').removeClass('active-grey');
+      }
+      if (distanceFromBottom == 0) {
+        $('nav ul #about-nav').removeClass('active-grey');
+        $('nav ul #projects-nav').addClass('active-grey');
+      }
     }
   });
 
 
-  // Scrolling
+  // Scrolling (desktop)
   $('#about-nav').click(function () {
     $('html, body').animate({
       scrollTop: $('#about').offset().top
@@ -77,18 +81,45 @@ $(document).ready(function (){
     }, 750);
     return false;
   });
-  // $('#contact-nav').click(function (){
-  //   $('html, body').animate({
-  //     scrollTop: $('#contact').offset().top
-  //   }, 750);
-  //   return false;
-  // });
   $('#home-nav').click(function (){
     $('html, body').animate({
       scrollTop: $('#home').offset().top
     }, 750);
     return false;
   });
+
+
+  // Scrolling (mobile)
+  $('a #about-nav').click(function() {
+    $('.nav-mobile').slideToggle('medium');  // close menu
+    $('html, body').animate({
+      scrollTop: $('#about').offset().top
+    }, 750);
+    return false;
+  });
+  $('a #projects-nav').click(function() {
+    $('.nav-mobile').slideToggle('medium');
+    $('html, body').animate({
+      scrollTop: $('#projects').offset().top - 8
+    }, 750);
+    return false;
+  });
+  $('a #home-nav').click(function() {
+    $('.nav-mobile').slideToggle('medium');
+    $('html, body').animate({
+      scrollTop: $('#home').offset().top
+    }, 750);
+    return false;
+  });
+
+
+  // Hamburger menu toggle for mobile
+  if ($(document).width() < 800) {
+    $('.nav-mobile').hide();
+    $('.handle').click(function() {
+      $('.nav-mobile').slideToggle('slow');
+    });
+  }
 
 
 });
